3dbf80be07b656905a25086dadf7487a36d0ddf6
ArmorBoyd <boydpoblete@gmail.com>
2025-09-19 20:50:50 +0800

    Finished Parts 1 and 2 errors of W01
    For Part 1, I reassigned the userName variable from const to let.
    For Part 2, I used the new Date () object to get the current date and time. Alternatively, I can also use the Date () object to show the date and time.

---
+++ debugging.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+let area = 0;
+function circleArea(radius) {
+  const area = radius * radius * PI;
+  return area
+}
+area1 = circleArea(3);
+area2 = circleArea(4)
+console.log(area1);
+console.log(area2);
