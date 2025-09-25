87fa2cab3c0a9891d66031060ff88d0093cc1413
ArmorBoyd <boydpoblete@gmail.com>
2025-09-25 21:22:09 +0800

    Oragnized the ways to this exercise

--- w02-task/dom-basics.js
+++ w02-task/dom-basics.js
@@ -2,24 +2,13 @@ const newParagrah = document.createElement("p");
 newParagrah.innerText = ("Added with JS");
 document.body.appendChild(newParagrah);
 
+// TOP Way
 const sect = document.querySelector("section");
 const para = document.createElement("p");
 para.textContent = "Added with JS";
 sect.appendChild(para);
 
-const image = document.createElement("img");
-image.setAttribute("src", "https://picsum.photos/200");
-image.setAttribute("alt", "Random Image from picsum");
-sect.appendChild(image);
-
-const newDiv = document.createElement("div");
-newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</></ul>"
-sect.appendChild(newDiv);
-
-const newSection = document.createElement("section");
-newSection.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>"
-document.body.appendChild(newSection)
-
+// TOP Way
 const newSections = document.createElement("section");
 const newH2 = document.createElement("h2");
 newH2.innerText = "DOM Basicssss"
@@ -28,3 +17,19 @@ const newP = document.createElement("p");
 newP.innerText = "This was added through Javascript!"
 newSections.appendChild(newP);
 sect.appendChild(newSections);
+
+// BYUI Way
+const image = document.createElement("img");
+image.setAttribute("src", "https://picsum.photos/200");
+image.setAttribute("alt", "Random Image from picsum");
+sect.appendChild(image);
+
+// BYUI Way
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</></ul>"
+sect.appendChild(newDiv);
+
+const newSection = document.createElement("section");
+newSection.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>"
+document.body.appendChild(newSection)
+
